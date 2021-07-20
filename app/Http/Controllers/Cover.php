24e7197c6deb7b047b158200198cd4cbcb507c89
Wiktor Kowalski <wiktorkowalski@kong.works>
2021-07-20 19:03:10 +0300

    Add laravel

---
+++ app/Http/Controllers/Cover.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Cover extends Controller
+{
+    public function index()
+    {
+        return 'This is Cover controller';
+    }
+}
